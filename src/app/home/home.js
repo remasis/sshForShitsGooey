b0e84542802781576cc9dbf28fd5c2e1cb21c204
Corey Thuen <corey.thuen@southforksecurity.com>
2014-12-10 14:39:27 -0700

    quick hackjob controller to display database info

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -15,22 +15,59 @@ angular.module('sshfs.home', [
 
 .run(function() {})
 
-.controller('homeCtrl', function HomeController($scope, titleService, $interval, $http) {
+.controller('homeCtrl', function HomeController($scope, titleService, $interval, $timeout, $http, $q, $anchorScroll, $location) {
 	console.log("hello from home");
 	titleService.set("SSH For Shits");
 
-	var content = "supernub@localhost ~/ $";
+	//location hack....
+	$location.hash('bottom');
 
-	$scope.type = "";
-	var i = 0;
+	function displayTyped(str) {
+		var i = 0;
+		var def = $q.defer();
 
-	var timer = $interval(function() {
-		if (i < content.length) {
-			$scope.type += content[i];
-		} else {
-			$interval.cancel(timer);
+		var timer = $interval(function() {
+			if (i < str.length) {
+				$scope.shelloutput[actindex].cmd += str[i];
+				$anchorScroll();
+			} else {
+				$interval.cancel(timer);
+				def.resolve();
+			}
+			i++;
+		}, 100);
+		return def.promise;
+	}
+
+	$http.get('/api/shells/recent')
+		.success(function(data) {
+			$scope.shell = data[0];
+			console.log($scope.shell);
+			shownext();
+		})
+		.error(function(data) {
+			console.error("ZOMG ERROR getting shell db:", data);
+		});
+
+	$scope.shelloutput = [];
+
+	var actindex = 0;
+
+	function shownext() {
+		if (actindex >= $scope.shell.shellactivity.length) {
+			console.log("all done with data");
+			return;
 		}
-		i++;
-	}, 100);
-
+		var act = $scope.shell.shellactivity[actindex];
+		$scope.shelloutput[actindex] = {
+			cmd: "",
+			resp: ""
+		};
+		displayTyped(act.cmd).then(function() {
+			$scope.shelloutput[actindex].resp = act.resp;
+			actindex = actindex + 1;
+			$timeout($anchorScroll, 1);
+			$timeout(shownext, 1000);
+		});
+	}
 });
